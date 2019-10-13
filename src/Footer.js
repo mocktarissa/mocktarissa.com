96c6fb07d81a217f2b3fc0bc3c69addd6897f240
Mocktar <mocktarissa@gmail.com>
2019-10-13 14:58:58 +0300

    Small ui modifications

--- src/Footer.js
+++ src/Footer.js
@@ -8,13 +8,15 @@ render(){
             <footer className="footer">
               <div className="content has-text-centered">
                 <p>
-                  <strong>Mocktar Issa </strong> by <a href="https://jgthms.com">Mocktar Issa</a>. The source code is licensed
+                  <strong>Mocktar Issa </strong> by <a href="https://mocktarissa.com">Mocktar Issa</a>. The source code is licensed
                   <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
                   is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
                 </p>
                 <p>
                     Built with love using <a href="https://bulma.io">
                     <img src="https://bulma.io/images/bulma-logo.png" width={50} height={28} />
+                    </a> And with React <a href="https://reactjs.org/">
+                    <img src="https://www.stickpng.com/assets/images/584830f5cef1014c0b5e4aa1.png" width={25} height={28} />
                     </a>
                 </p>
               </div>
