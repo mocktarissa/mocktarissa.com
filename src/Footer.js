69727bccc1ebec4b9df117e99fb4e7fe9a4018d0
Mocktar <mocktarissa@gmail.com>
2019-12-10 09:45:22 +0300

    Footer added

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,12 @@ class Footer extends React.Component{
 render(){
     return(
      <div class="footer">
-
+         <p>&copy; Mocktar Issa</p>
+        <li><a href="https://github.com/mocktarissa"><i class="fab fa-github"></i>Github</a></li>
+        <li><a href="https://medium.com/@mocktarissa"><i class="fab fa-medium"></i>Medium</a></li>
+        <li><a href=""><i class="fab fa-instagram"></i>instagram</a></li>
+        <li><a href=""><i class="fab fa-twitter"></i>Twitter</a></li>
+        <li><a href=""><i class="fab fa-youtube"></i>Youtube</a></li>
      </div>     );
    
 }
