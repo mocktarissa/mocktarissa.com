f360bc552fd6f907fcc0a09e14bf01255ad6df7b
Mocktar <mocktarissa@gmail.com>
2020-07-05 08:48:24 +0300

    Adding project list

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,7 @@ render(){
         <li><a href="https://github.com/mocktarissa"><i class="fab fa-github"></i>Github</a></li>
         <li><a href="https://medium.com/@mocktarissa"><i class="fab fa-medium"></i>Medium</a></li>
         <li><a href="https://www.instagram.com/mocktar_issa/"><i class="fab fa-instagram"></i>Instagram</a></li>
-        <li><a href="https://medium.com/@mocktarissa"><i class="fab fa-twitter"></i>Twitter</a></li>
+        <li><a href="https://twitter.com/mocktarissa"><i class="fab fa-twitter"></i>Twitter</a></li>
         <li><a href="https://www.linkedin.com/in/mocktar-issa-30a9674a/"><i class="fab fa-linkedin"></i>Youtube</a></li>
         </div> 
                
