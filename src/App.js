54e6efd063c21d476e1d9e72305393a266be65d2
Mocktar <44744049+mocktarissa@users.noreply.github.com>
2021-09-21 00:46:23 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import "./App.css";
 import Navbar from "./Navbar";
 
 import Footer from "./Footer";
-import Portfolio from "./containers/Portfolio";
-import { Route, Switch } from "react-router-dom";
-import Contact from "./containers/Contact";
-import Home from "./containers/Home";
-import Shop from "./containers/Shop";
-import AboutMe from "./containers/AboutMe";
+// import Portfolio from "./containers/Portfolio";
+// import { Route, Switch } from "react-router-dom";
+// import Contact from "./containers/Contact";
+// import Home from "./containers/Home";
+// import Shop from "./containers/Shop";
+// import AboutMe from "./containers/AboutMe";
 import Body from "./Body";
-import UnderConstruction from "./components/UnderConstruction";
+// import UnderConstruction from "./components/UnderConstruction";
 
 class App extends React.Component {
   constructor(props) {
