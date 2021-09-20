9a362f2b199b8a47b4c1a6d57ecd7ac8e3ca2bdd
Mocktar <44744049+mocktarissa@users.noreply.github.com>
2021-09-21 00:45:46 +0100

    working website

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./Navbar";
 
-// import Footer from './Footer';
-// import Portfolio from './containers/Portfolio';
-// import {Route, Switch} from 'react-router-dom';
-// import Contact from './containers/Contact';
-// import Home from './containers/Home';
-// import Shop from './containers/Shop';
-// import AboutMe from './containers/AboutMe';
-// import Body from "./Body";
+import Footer from "./Footer";
+import Portfolio from "./containers/Portfolio";
+import { Route, Switch } from "react-router-dom";
+import Contact from "./containers/Contact";
+import Home from "./containers/Home";
+import Shop from "./containers/Shop";
+import AboutMe from "./containers/AboutMe";
+import Body from "./Body";
 import UnderConstruction from "./components/UnderConstruction";
 
 class App extends React.Component {
@@ -29,16 +29,16 @@ class App extends React.Component {
     return (
       <div className="App">
         <Navbar background={this.state.background ? "dark" : " "} />
-        {/* <Body></Body> */}
-        <UnderConstruction></UnderConstruction>
-        {/* <Switch> 
-         <Route exact path="/" render={()=><Home/>}></Route>
-        <Route exact path="/contact" render={()=><Contact/>}/>
-        <Route exact path="/portfolio" render={()=><Portfolio/>}/>
-        <Route exact path="/shop" render={()=><Shop/>}/>
-        <Route exact path="/about" render={()=><AboutMe/>}/>
-       </Switch>                     
-        <Footer/> */}
+        <Body></Body>
+        {/* <UnderConstruction></UnderConstruction> */}
+        {/* <Switch>
+          <Route exact path="/" render={() => <Home />}></Route>
+          <Route exact path="/contact" render={() => <Contact />} />
+          <Route exact path="/portfolio" render={() => <Portfolio />} />
+          <Route exact path="/shop" render={() => <Shop />} />
+          <Route exact path="/about" render={() => <AboutMe />} />
+        </Switch> */}
+        <Footer />
       </div>
     );
   }
